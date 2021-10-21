21ee6723e8650d84e143f8ebdf4320b01e003c16
Saif Mahmud <60409889+vmsaif@users.noreply.github.com>
2021-10-21 14:25:32 -0400

    updated

--- src/A5MahmudSaif.java
+++ src/A5MahmudSaif.java
@@ -1,13 +1,9 @@
 
-
 /**
- * COMP 2140   		SECTION A01
- * INSTRUCTOR   	Cuneyt Akcora
- * Assignment       5
  * @author			Saif Mahmud
- * @id				7808507
  * @version      	04/06/2020
  */
+
 import javafx.util.Pair;
 
 import java.math.BigInteger;
